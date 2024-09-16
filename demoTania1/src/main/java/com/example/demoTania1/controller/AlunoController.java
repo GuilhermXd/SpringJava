af2d9fcda1f48ea0c115580af94d923699c34d38
GuilhermXd <159050854+GuilhermXd@users.noreply.github.com>
2024-09-16 15:19:23 -0300

    Add files via upload

---
+++ demoTania1/src/main/java/com/example/demoTania1/controller/AlunoController.java
@@ -0,0 +1,31 @@
+package com.example.demoTania1.controller;
+
+
+import com.example.demoTania1.model.Aluno;
+import com.example.demoTania1.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="=/apiAluno")
+public class AlunoController {
+
+    @Autowired
+    AlunoRepository alRepo;
+
+
+    @GetMapping(value="/alunos")
+    public List<Aluno> listarAlunos()
+    {
+        return alRepo.findAll();
+    }
+
+
+
+
+
+}
---
+++ demoTania1/src/test/java/com/example/demoTania1/DemoTania1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoTania1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTania1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
