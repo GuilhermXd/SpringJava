6430c5556ca95f9eed78ad15ec65e661d69ce2ac
GuilhermXd <159050854+GuilhermXd@users.noreply.github.com>
2024-09-23 14:45:00 -0300

    Add files via upload

---
+++ SpringJava-main/demoTania1/src/main/java/com/example/demoTania1/DemoTania1Application.java
@@ -0,0 +1,13 @@
+package com.example.demoTania1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTania1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTania1Application.class, args);
+	}
+
+}
---
+++ SpringJava-main/demoTania1/src/main/java/com/example/demoTania1/repository/AlunoRepository.java
@@ -0,0 +1,8 @@
+package com.example.demoTania1.repository;
+
+import com.example.demoTania1.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository <Aluno, Integer> {
+
+}
