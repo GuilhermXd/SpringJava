21594a1ff81da9214e5b423546fb4f566174c9dc
GuilhermXd <159050854+GuilhermXd@users.noreply.github.com>
2024-09-27 11:45:26 -0300

    Add files via upload

--- demoTania1/src/main/java/com/example/demoTania1/controller/AlunoController.java
+++ demoTania1/src/main/java/com/example/demoTania1/controller/AlunoController.java
@@ -4,14 +4,13 @@ package com.example.demoTania1.controller;
 import com.example.demoTania1.model.Aluno;
 import com.example.demoTania1.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(value="=/apiAluno")
+@RequestMapping(value="/apiAlunoo")
 public class AlunoController {
 
     @Autowired
@@ -24,8 +23,33 @@ public class AlunoController {
         return alRepo.findAll();
     }
 
+    @GetMapping(value="/alunos/{ra}")
+    public Optional<Aluno> FindID (@PathVariable(value = "ra")int ra)
+    {
+        return alRepo.findById(ra);
+    }
 
+    @PostMapping(value="/inserir")
+    public void InserirAluno (@RequestBody Aluno al)
+    {
+        alRepo.save(al);
+    }
 
+    @DeleteMapping(value = "/delete")
+    public void deletarAluno(@RequestBody Aluno al)
+    {
+        alRepo.delete(al);
+    }
 
+    @DeleteMapping(value = "/deleteID/{ra}")
+    public void deletebyID(@PathVariable(value = "ra")int ra)
+    {
+        alRepo.deleteById(ra);
+    }
 
+    @PutMapping(value = "update")
+    private void AtualizarAluno(@RequestBody Aluno al)
+    {
+        alRepo.save(al);
+    }
 }
